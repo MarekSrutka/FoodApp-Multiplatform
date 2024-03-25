853bdb2c11993e97527208c735e4dc48ddd7e297
MarekSrutka <spiritecz@icloud.com>
2024-03-25 17:00:01 +0100

    createApp

---
+++ FoodApp/FoodApp/FoodAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodAppApp.swift
+//  FoodApp
+//
+//  Created by Marek Srutka on 25.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct FoodAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
